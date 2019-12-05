59d2dc5be082c6438365d23c381b2856a8186a3d
Gyeongsik Kim <gyungdal@the-s.kr>
2019-12-05 11:24:19 +0900

    sensor 파서 추가

--- lib/oxygen_read/Oxygen.hpp
+++ lib/oxygen_read/Oxygen.hpp
@@ -1,41 +1,58 @@
 #ifndef __OXYGEN_HPP__
 #define __OXYGEN_HPP__
-
+/***
+ * ESP32 용 산소 센서 라이브러리
+ */
 #include <Arduino.h>
 #include <inttypes.h>
+#include <stdbool.h>
+#include <vector>
+#include <string>
+
+using namespace std;
 
 typedef enum {
     STREAM = 0,
     POLL = 1,
-    OFF = 2
+    OFF = 2,
+
+    DATE_OF_MANUFACTURE = STREAM,
+    SERIAL_NUMBER = POLL,
+    SOFTWARE_REVISION = OFF
 } OxygenOutputMode_t;
-‘
+
 typedef enum {
-    SET_OUTPUT_MODE = 'M',
     REQUEST_PPO2 = 'O',
     REQUEST_O2 = '%',
     REQUEST_TEMPERATURE = 'T',
     REQUEST_BAROMETRIC = 'P',
+} OxygenSensorRequest_t;
+
+typedef enum {
+    SET_OUTPUT_MODE = 'M',
     REQUEST_ALL = 'A',
     SENSOR_STATUS = 'e',
     SENSOR_INFO = '#'
 } OxygenRequest_t;
 
-class Oxygen(){
+typedef struct {
+    uint8_t* data;
+    uint32_t len;
+} OxygenData_t;
+class Oxygen{
     private: 
-        typedef struct {
-            uint8_t* data;
-            uint32_t len;
-        } OxygenData;
-
-        void reqest(uint8_t* data, uint32_t len);
-        int recv(uint8_t* data, uint32_t* len);
+        OxygenData_t request(OxygenData_t packet);
+        OxygenData_t request(uint8_t command);
+        OxygenData_t request(uint8_t command, uint8_t arg);
     public:
-        Oxygen::Oxygen(int rx, int tx){
+        Oxygen(int rx, int tx){
             Serial2.begin(9600, SERIAL_8N1, rx, tx);
-            stream = Serial2;
         }
-
+        double sensorValue(OxygenSensorRequest_t sensor);
+        double* allSensorValue();
+        bool sensorStatus();
+        void setOutputMode(OxygenOutputMode_t type);
+        string sensorInfo(OxygenOutputMode_t type);
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,60 @@
 #include <Arduino.h>
+#include <cstring>
+#include <cstdlib>
+
+const int SENSOR_TX_PIN = 20;
+const int SENSOR_RX_PIN = 21;
+
+typedef struct {
+  double ppO2;
+  double temp;
+  double barometric;
+  double o2;
+  bool isOk;
+} sensor_t;
+
+volatile sensor_t sensor;
+
+void IRAM_ATTR SerialEvent2(){
+  char temp[256] = {0, };
+  Serial2.readBytesUntil('\n', temp, 256);
+  char* token = strtok(temp, " ");
+  while(token != NULL){
+    char command = token[0];
+    token = strtok(NULL, " ");
+    if(command == 'e'){
+      int code = atoi(token);
+      sensor.isOk = (code == 0);
+    }else{
+      double* value;
+      switch(command){
+        case 'O' : {
+          value = &(sensor.ppO2);
+          break;
+        }
+        case 'T' : {
+          value = &(sensor.temp);
+          break;
+        }
+        case 'P' : {
+          value = &(sensor.barometric);
+          break;
+        }
+        case '%' : {
+          value = &(sensor.o2);
+          break;
+        }
+      }
+      *value = atof(token);
+    }
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial2.begin(9600, SERIAL_8N1, SENSOR_RX_PIN, SENSOR_TX_PIN);
+  Serial2.setInterrupt(&SerialEvent2);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
 }
