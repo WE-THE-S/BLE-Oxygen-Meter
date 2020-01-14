920c698e613e04f00d3214d555d048da658a9ff0
Gyeongsik Kim <gyungdal@the-s.kr>
2020-01-14 14:51:47 +0900

    u8g2 ㄹㅏ이브러리 추가
    10초 Deep_sleep 추가

--- src/main.cpp
+++ src/main.cpp
@@ -2,128 +2,113 @@
 #include <cstring>
 #include <cstdlib>
 #include <HardwareSerial.cpp>
+#include <esp_log.h>
 
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
+#include <U8g2lib.h>
 
-BLEServer* pServer = NULL;
-BLECharacteristic* pCharacteristic = NULL;
-bool deviceConnected = false;
-bool oldDeviceConnected = false;
-uint32_t value = 0;
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
+#define uS_TO_S_FACTOR 1000 * 1000
+#define TIME_TO_SLEEP  10
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 
-const int SENSOR_TX_PIN = 20;
-const int SENSOR_RX_PIN = 21;
+const int SENSOR_TX_PIN = 35;
+const int SENSOR_RX_PIN = 32;
 
-typedef struct {
-  double ppO2;
-  double temp;
-  double barometric;
-  double o2;
-  bool isOk;
+typedef union {
+  struct{
+    float ppO2;
+    float temp;
+    float barometric;
+    float o2;
+    bool isOk;
+  };
+  uint8_t bytes[13];
 } sensor_t;
 
 volatile sensor_t sensor;
 
-
-class MyServerCallbacks: public BLEServerCallbacks {
-  void onConnect(BLEServer* pServer) {
-    deviceConnected = true;
-    BLEDevice::startAdvertising();
+void readSensor(){
+  char temp[256] = {0, };
+  Serial2.setTimeout(2000);
+  Serial2.readBytesUntil('\n', temp, 256);
+  char* token = strtok(temp, " ");
+  while(token != NULL){
+    ESP_LOGD("raw", "%s", token);
+    char command = token[0];
+    
+    if(command == 'e'){
+      int code = atoi(token);
+      sensor.isOk = (code == 0);
+    }else{
+      volatile float* value = nullptr;
+      switch(command){
+        case 'O' : {
+          value = &(sensor.ppO2);
+          break;
+        }
+        case 'T' : {
+          value = &(sensor.temp);
+          break;
+        }
+        case 'P' : {
+          value = &(sensor.barometric);
+          break;
+        }
+        case '%' : {
+          value = &(sensor.o2);
+          break;
+        }
+      }
+      if(value != nullptr){
+        *value = atof(token);
+      }
+    }
   }
-
-  void onDisconnect(BLEServer* pServer) {
-    deviceConnected = false;
-  }
-};
+}
 
 void setup() {
+  sensor.isOk = false;
+  u8g2.begin();
   Serial.begin(115200);
   Serial2.begin(9600, SERIAL_8N1, SENSOR_RX_PIN, SENSOR_TX_PIN);
-
+  readSensor();
   BLEDevice::init("Oxygen Meter");
-  pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
-
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-
-  pCharacteristic = pService->createCharacteristic(
+  BLEServer* pServer = BLEDevice::createServer();
+  BLEService* pService = pServer->createService(SERVICE_UUID);
+  BLECharacteristic* pCharacteristic = pService->createCharacteristic(
                       CHARACTERISTIC_UUID,
-                      BLECharacteristic::PROPERTY_READ   |
-                      BLECharacteristic::PROPERTY_WRITE  |
-                      BLECharacteristic::PROPERTY_NOTIFY |
-                      BLECharacteristic::PROPERTY_INDICATE
-                    );
+                      BLECharacteristic::PROPERTY_READ
+                      );
 
-  pCharacteristic->addDescriptor(new BLE2902());
-
+  pCharacteristic->setValue(const_cast<uint8_t*>(sensor.bytes), sizeof(sensor_t));
   pService->start();
 
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
   pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(false);
-  pAdvertising->setMinPreferred(0x0); 
+  pAdvertising->setMinPreferred(0);
+   
   BLEDevice::startAdvertising();
-  ESP_LOGI("Waiting a client connection to notify...");
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+  ESP_LOGI("Notify", "Waiting a client connection to notify...");
+  esp_deep_sleep_start();
 }
 
 void loop() {
-    char temp[256] = {0, };
-    Serial2.readBytesUntil('\n', temp, 256);
-    char* token = strtok(temp, " ");
-    while(token != NULL){
-      char command = token[0];
-      token = strtok(NULL, " ");
-      if(command == 'e'){
-        int code = atoi(token);
-        sensor.isOk = (code == 0);
-      }else{
-        volatile double* value = nullptr;
-        switch(command){
-          case 'O' : {
-            value = &(sensor.ppO2);
-            break;
-          }
-          case 'T' : {
-            value = &(sensor.temp);
-            break;
-          }
-          case 'P' : {
-            value = &(sensor.barometric);
-            break;
-          }
-          case '%' : {
-            value = &(sensor.o2);
-            break;
-          }
-        }
-        if(value != nullptr){
-          *value = atof(token);
-        }
-      }
-    }
-    if (deviceConnected) {
-        pCharacteristic->setValue((uint8_t*)&value, 4);
-        pCharacteristic->notify();
-        value++;
-    }
-    
-    if (!deviceConnected && oldDeviceConnected) {
-        delay(500);
-        pServer->startAdvertising(); 
-        Serial.println("start advertising");
-        oldDeviceConnected = deviceConnected;
-    }
-    
-    if (deviceConnected && !oldDeviceConnected) {
-        oldDeviceConnected = deviceConnected;
-    }
 }
