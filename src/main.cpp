6416b57f5ea5a3d3bf22f19a8ab31408d0c268ce
Gyeongsik Kim <gyungdal@the-s.kr>
2020-01-17 10:46:24 +0900

    WiFi Disable
    BLE TX Power 설정

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,13 @@
 #include <cstring>
 #include <esp_log.h>
 
+#include "esp_bt.h"
+#include "esp_wifi.h"
 #include "esp_bt_defs.h"
 #include "esp_bt_main.h"
 #include "esp_gap_ble_api.h"
 #include "esp_gatts_api.h"
+#include "driver/adc.h"
 #include <U8g2lib.h>
 
 #ifdef U8X8_HAVE_HW_SPI
@@ -18,14 +21,14 @@
 #endif
 
 #define uS_TO_S_FACTOR 1000 * 1000
+#define mS_TO_S_FACTOR 1000
 #define TIME_TO_SLEEP 10
 
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/U8X8_PIN_NONE);
-RTC_DATA_ATTR static uint32_t bootcount; // remember number of boots in RTC Memory
 
 const int SENSOR_TX_PIN = 35;
 const int SENSOR_RX_PIN = 32;
-
+const int SENSOR_POWER_PIN = 13;
 typedef union {
 	struct {
 		float temp;
@@ -51,7 +54,8 @@ esp_ble_adv_data_t adv_config = {
 	.p_service_data = NULL,
 	.service_uuid_len = 0,
 	.p_service_uuid = NULL,
-	.flag = (ESP_BLE_ADV_FLAG_NON_LIMIT_DISC)};
+	.flag = (ESP_BLE_ADV_FLAG_NON_LIMIT_DISC)
+};
 
 esp_ble_adv_params_t adv_param = {
 	.adv_int_min = 512,
@@ -87,21 +91,18 @@ char *barray2hexstr(uint8_t *data, size_t datalen) {
 
 void print_wakeup_reason() {
   const char* TAG = "WakeUpTag";
-  esp_sleep_wakeup_cause_t wakeup_reason;
-
-	wakeup_reason = esp_sleep_get_wakeup_cause();
-
+  esp_sleep_wakeup_cause_t wakeup_reason = esp_sleep_get_wakeup_cause();
+  u8g2.begin();
 	switch (wakeup_reason) {
     case ESP_SLEEP_WAKEUP_UNDEFINED : {
-	    u8g2.begin();
       ESP_LOGD(TAG, "BOOT");
       break;
     }
 		case ESP_SLEEP_WAKEUP_TIMER:{
-      u8g2.clearBuffer();
       u8g2.setFont(u8g2_font_ncenB14_tr);
       u8g2.drawStr(0,20, "Hello World!");
       u8g2.sendBuffer();
+      u8g2.setPowerSave(1);
 			ESP_LOGD(TAG, "Wakeup caused by timer");
 			break;
     }
@@ -169,12 +170,16 @@ static void gap_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *pa
 }
 
 void setup() {
-  const char* TAG = "Bluetooth";
+	adc_power_off();
+	const char* TAG = "Bluetooth";
+	ESP_LOGI("BootMode", "After Deep Sleep");
+	esp_wifi_deinit();
+	esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_DEFAULT, ESP_PWR_LVL_N12);
+	esp_ble_tx_power_set(ESP_BLE_PWR_TYPE_ADV, ESP_PWR_LVL_P9);
+	
 	sensor.isOk = false;
 	Serial2.begin(9600, SERIAL_8N1, SENSOR_RX_PIN, SENSOR_TX_PIN);
-  print_wakeup_reason();
-	readSensor();
-
+	print_wakeup_reason();
 	if (!btStarted() && !btStart()) {
 		ESP_LOGD(TAG, "BT Start Fail");
 	}
@@ -189,6 +194,7 @@ void setup() {
 			ESP_LOGD(TAG, "enable failed");
 		}
 	}
+	readSensor();
 	//D000 0000CDCC A441 F7030000 EC
 	char *hex = barray2hexstr(sensor.bytes, 14);
 	ESP_LOGD(TAG, "%s", hex);
@@ -198,9 +204,8 @@ void setup() {
 	adv_config.manufacturer_len = 13;
 	esp_ble_gap_config_adv_data(&adv_config);
 	esp_ble_gap_register_callback(gap_handler);
-	bootcount++;
 	ESP_LOGD(TAG, "Advertizing started...");
-	delay(100);
+	digitalWrite(SENSOR_POWER_PIN, LOW);
 	ESP_LOGD(TAG, "enter deep sleep");
 	esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
 	esp_deep_sleep_start();
