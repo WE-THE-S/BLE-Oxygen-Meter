e12ee58ab2fdb51c7295cb141b0b21bf2e109865
Gyeongsik Kim <gyungdal@the-s.kr>
2020-03-12 13:17:13 +0900

    에러 체크 로직 추가 및 로직 간단화 불필요한 if문 switch로 통합

--- src/ble.cpp
+++ src/ble.cpp
@@ -5,21 +5,26 @@ esp_err_t BLE::broadcast(sensor_t *sensor) {
 	if (!btStarted() && !btStart()) {
 		ESP_LOGD(TAG, "BT Start Fail");
 	}
-	esp_bluedroid_status_t bt_state = esp_bluedroid_get_status();
-	if (bt_state == ESP_BLUEDROID_STATUS_UNINITIALIZED) {
-		if (esp_bluedroid_init()) {
-			ESP_LOGD(TAG, "init failed");
+	switch(esp_bluedroid_get_status()){
+		case ESP_BLUEDROID_STATUS_ENABLED : {
+			break;
+		}
+		case ESP_BLUEDROID_STATUS_UNINITIALIZED : 
+			ESP_ERROR_CHECK(esp_bluedroid_init());
+		default : {
+			ESP_ERROR_CHECK(esp_bluedroid_enable());
+			break;
 		}
 	}
-	if (bt_state != ESP_BLUEDROID_STATUS_ENABLED) {
-		if (esp_bluedroid_enable()) {
-			ESP_LOGD(TAG, "enable failed");
-		}
-	}
-	esp_ble_gap_set_device_name("Oxygen Meter");
+	
+	//데이터 설정
 	adv_config.p_manufacturer_data = sensor->bytes;
 	adv_config.manufacturer_len = 13;
-	esp_ble_gap_config_adv_data(&adv_config);
-	esp_ble_gap_register_callback(gap_handler);
+
+	//BLE 에러 체크 및 설정
+	ESP_ERROR_CHECK(esp_ble_gap_set_device_name("Oxygen Meter"));
+	ESP_ERROR_CHECK(esp_ble_gap_config_adv_data(&adv_config));
+	ESP_ERROR_CHECK(esp_ble_gap_register_callback(gap_handler));
+
 	return ESP_OK;
 }
